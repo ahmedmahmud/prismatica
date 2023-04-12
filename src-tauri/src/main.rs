a86a169906ce03bd449e41fbc79e825cd53f0e91
polycrylate <ahmedmahmud12971@gmail.com>
2023-04-12 23:20:59 +0100

    feat: :sparkles: add basic conversion to catppuccin to rust backend

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,34 @@
   windows_subsystem = "windows"
 )]
 
+use magick_rust::{MagickWand, magick_wand_genesis, MagickError};
+use std::sync::Once;
+
+static START: Once = Once::new();
+
+// Convert image using hald clut
+fn convert() -> Result<(), MagickError> {
+  let image_wand = MagickWand::new();
+  let lut_wand = MagickWand::new();
+  
+  image_wand.read_image("test.jpg")?;
+  lut_wand.read_image("flavors/noise-4/oled.png")?;
+
+  image_wand.hald_clut_image(&lut_wand)?;
+
+  image_wand.write_image("oled-test.jpg")
+}
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+  // Initialize magick wand
+  START.call_once(|| {
+    magick_wand_genesis();
+  });
+
+  convert();
+
+  // tauri::Builder::default()
+  //   .run(tauri::generate_context!())
+  //   .expect("error while running tauri application");
+
 }
