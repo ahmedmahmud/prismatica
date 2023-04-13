9cb174ce3d8a755a23f72fd732afccdc3221cd47
polycrylate <ahmedmahmud12971@gmail.com>
2023-04-13 02:00:50 +0100

    feat: :sparkles: add proper CLI support to interact with conversion

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,21 +4,34 @@
 )]
 
 use magick_rust::{MagickWand, magick_wand_genesis, MagickError};
-use std::sync::Once;
+use serde_json::Value;
+use std::{sync::Once, path::Path};
+use include_dir::{include_dir, Dir};
 
+static PROJECT_DIR: Dir = include_dir!("$CARGO_MANIFEST_DIR/flavors");
 static START: Once = Once::new();
 
+fn gen_new_name(file_path: String, palette: String) -> String {
+  let path = Path::new(&file_path);
+  let file_stem = path.file_stem().unwrap().to_str().unwrap();
+  let extension = path.extension().unwrap().to_str().unwrap();
+
+  format!("{palette}-{file_stem}.{extension}")
+}
+
 // Convert image using hald clut
-fn convert() -> Result<(), MagickError> {
+fn convert(file_path: String, noise: String, palette: String) -> Result<(), MagickError> {
   let image_wand = MagickWand::new();
-  let lut_wand = MagickWand::new();
+  image_wand.read_image(&file_path)?;
   
-  image_wand.read_image("test.jpg")?;
-  lut_wand.read_image("flavors/noise-4/oled.png")?;
+  let lut_wand = MagickWand::new();
+  let lut_bytes = PROJECT_DIR.get_file(format!("noise-{noise}/{palette}.png")).unwrap().contents();
+  lut_wand.read_image_blob(lut_bytes)?;
 
   image_wand.hald_clut_image(&lut_wand)?;
 
-  image_wand.write_image("oled-test.jpg")
+  let file_name = gen_new_name(file_path, palette);
+  image_wand.write_image(&file_name)
 }
 
 fn main() {
@@ -27,10 +40,47 @@ fn main() {
     magick_wand_genesis();
   });
 
-  convert();
+  tauri::Builder::default()
+    .setup(|app| {
+      let handle = app.handle();
+      match app.get_cli_matches() {
+        Ok(mut matches) => {
+          let file_path = matches.args.remove("source").unwrap();
+          let noise = matches.args.remove("noise").unwrap();
+          let palette = matches.args.remove("palette").unwrap();
 
-  // tauri::Builder::default()
-  //   .run(tauri::generate_context!())
-  //   .expect("error while running tauri application");
+          if file_path.occurrences != 0 || noise.occurrences != 0 || palette.occurrences != 0 {
+            let file_path = match file_path.value {
+              Value::String(v) => v,
+              _ => panic!("File path provided is not a string")
+            };
+
+            let noise = match noise.value {
+              Value::String(v) => v,
+              _ => panic!("Noise provided is not a string")
+            };
+
+            let palette = match palette.value {
+              Value::String(v) => v,
+              _ => panic!("Palette provided is not a string")
+            };
+
+            match convert(file_path, noise, palette).err() {
+              Some(err) => panic!("{err}"),
+              None => {}
+            };
+
+            handle.exit(0);
+          }
+          // No args provided, continue with frontend launch
+        }
+        Err(err) => {
+          panic!("{err}");
+        }
+      }
+      Ok(())
+    })
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
 
 }
