cf0a0421b303f5f273ab7566b501c156391a37d1
polycrylate <ahmedmahmud12971@gmail.com>
2023-04-13 14:04:17 +0100

    dev(ui): :art: add styling for image view page, add upload page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,20 @@
   import "../app.css";
 </script>
 
-<slot />
+<div class="bg-base-300 flex flex-col min-h-screen">
+  <div class="navbar text-white">
+    <div class="navbar-start">
+      <button class="btn btn-ghost btn-circle">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+      </button>
+    </div>
+    <div class="navbar-center">
+      <h1 class="btn btn-ghost normal-case text-xl">prismatica</h1>
+    </div>
+    <div class="navbar-end" />
+  </div>
+  
+  <div class="rounded-3xl w-full bg-base-100 flex-grow flex flex-col">
+    <slot />
+  </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,86 +1,10 @@
-<script>
-  // When using the Tauri API npm package:
-  import { invoke } from '@tauri-apps/api'
-
-  let url = ""
-
-  let get_image = async () => {
-    const data = await invoke('get_image');
-    const array = new Uint8Array(data);
-    const blob = new Blob([array], { type: 'image/jpeg' });
-    url = URL.createObjectURL(blob);
-  }
-  
-  get_image();
-</script>
-
-<div class="tabs tabs-boxed flex-initial">
-  <a class="tab flex-1">Latte</a> 
-  <a class="tab tab-active flex-1">Frappe</a> 
-  <a class="tab flex-1">Macchiato</a>
-  <a class="tab flex-1">Mocha</a>
+<div class="flex h-full justify-center items-center flex-grow">
+  <div class="text-center mb-10">
+    <h1 class="text-5xl mb-10 text-opacity-60 text-white">Transform image</h1>
+    <select class="select select-lg select-bordered w-full max-w-xs mb-10">
+      <option disabled selected>Palette</option>
+      <option>Catppuccin</option>
+    </select>
+    <input type="file" class="file-input w-full max-w-xs mb-10" />
+  </div>
 </div>
-
-<div class="columns-md gap-20 p-20">
-  <div class="pb-20 break-inside-avoid">
-    <img src={url} class="mb-5 rounded-lg shadow-xl" alt="">
-    <div class="flex gap-5">
-      <button class="btn btn-primary flex-1 shadow-xl">Download</button>
-      <label class="btn btn-secondary flex-1 shadow-xl" for="my-modal-4">View</label>
-    </div>
-  </div>
-  <div class="pb-20 break-inside-avoid">
-    <img src={url} class="mb-5 rounded-lg shadow-xl" alt="">
-    <div class="flex gap-5">
-      <button class="btn btn-primary flex-1 shadow-xl">Download</button>
-      <label class="btn btn-secondary flex-1 shadow-xl" for="my-modal-4">View</label>
-    </div>
-  </div>
-  <div class="pb-20 break-inside-avoid">
-    <img src={url} class="mb-5 rounded-lg shadow-xl" alt="">
-    <div class="flex gap-5">
-      <button class="btn btn-primary flex-1 shadow-xl">Download</button>
-      <label class="btn btn-secondary flex-1 shadow-xl" for="my-modal-4">View</label>
-    </div>
-  </div>
-  <div class="pb-20 break-inside-avoid">
-    <img src={url} class="mb-5 rounded-lg shadow-xl" alt="">
-    <div class="flex gap-5">
-      <button class="btn btn-primary flex-1 shadow-xl">Download</button>
-      <label class="btn btn-secondary flex-1 shadow-xl" for="my-modal-4">View</label>
-    </div>
-  </div>
-  <div class="pb-20 break-inside-avoid">
-    <img src={url} class="mb-5 rounded-lg shadow-xl" alt="">
-    <div class="flex gap-5">
-      <button class="btn btn-primary flex-1 shadow-xl">Download</button>
-      <label class="btn btn-secondary flex-1 shadow-xl" for="my-modal-4">View</label>
-    </div>
-  </div>
-  <div class="pb-20 break-inside-avoid">
-    <img src={url} class="mb-5 rounded-lg shadow-xl" alt="">
-    <div class="flex gap-5">
-      <button class="btn btn-primary flex-1 shadow-xl">Download</button>
-      <label class="btn btn-secondary flex-1 shadow-xl" for="my-modal-4">View</label>
-    </div>
-  </div>
-  <div class="pb-20 break-inside-avoid">
-    <img src={url} class="mb-5 rounded-lg shadow-xl" alt="">
-    <div class="flex gap-5">
-      <button class="btn btn-primary flex-1 shadow-xl">Download</button>
-      <label class="btn btn-secondary flex-1 shadow-xl" for="my-modal-4">View</label>
-    </div>
-  </div>
-
-</div>
-
-<!-- Put this part before </body> tag -->
-<input type="checkbox" id="my-modal-4" class="modal-toggle" />
-<label for="my-modal-4" class="modal cursor-pointer p-10">
-  <img src={url} class="rounded-lg shadow-xl max-h-full" alt="">
-
-  <!-- <label class="modal-box relative" for="">
-    <h3 class="text-lg font-bold">Congratulations random Internet user!</h3>
-    <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
-  </label> -->
-</label>
